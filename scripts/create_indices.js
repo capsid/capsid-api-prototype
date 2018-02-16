72f9d813c9d03f143fdddc1f9c39e4956e7c7007
Jordan Nieuwhof <jgnieuwhof@gmail.com>
2018-02-16 18:20:48 -0500

    :sparkles: added samples to schema and seed data

--- scripts/create_indices.js
+++ scripts/create_indices.js
@@ -1,10 +1,18 @@
 import client from "@capsid/query/client";
 import projects from "@capsid/mappings/projects";
+import samples from "@capsid/mappings/samples";
 
-client.indices.create(
-  {
-    index: "projects",
-    body: { mappings: { _doc: projects } }
-  },
-  () => console.log("Projects index created.")
+const mappings = {
+  projects,
+  samples
+};
+
+Object.keys(mappings).forEach(key =>
+  client.indices.create(
+    {
+      index: key,
+      body: { mappings: { _doc: mappings[key].mapping } }
+    },
+    () => console.log(`${key} index created.`)
+  )
 );
--- src/mappings/projects.js
+++ src/mappings/projects.js
@@ -1,11 +1,14 @@
 export default {
-  properties: {
-    id: { type: "keyword" },
-    description: { type: "text" },
-    roles: { type: "keyword" },
-    label: { type: "keyword" },
-    version: { type: "long" },
-    wikiLink: { type: "text" },
-    name: { type: "keyword" }
-  }
+  mapping: {
+    properties: {
+      id: { type: "keyword" },
+      description: { type: "text" },
+      roles: { type: "keyword" },
+      label: { type: "keyword" },
+      version: { type: "long" },
+      wikiLink: { type: "text" },
+      name: { type: "keyword" }
+    }
+  },
+  pluralFields: []
 };
