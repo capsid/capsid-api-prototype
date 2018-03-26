d8645372b5ab7b39da61685edf940dadebe8e8bd
Jordan Nieuwhof <jgnieuwhof@gmail.com>
2018-03-26 17:16:14 -0400

    :bug: rolling back query, didn't work in prod

--- scripts/sync.js
+++ scripts/sync.js
@@ -1,6 +1,3 @@
-import mongoose from "mongoose";
-import fs from "fs";
-
 import { createIndices } from "./utils";
 import { connect, close } from "@capsid/mongo/utils";
 
@@ -13,8 +10,6 @@ import { Statistics } from "@capsid/mongo/schema/statistics";
 
 const models = [Project, Sample, Alignment, MappedRead, Genome, Statistics];
 
-const anchorFromName = name => `${name}.anchor`;
-
 const main = async () => {
   await connect();
 
@@ -24,49 +19,17 @@ const main = async () => {
 
   await Promise.all(
     models
-      .map(Model => ({
-        name: Model.collection.name,
-        anchor: anchorFromName(Model.collection.name),
-        Model
-      }))
+      .map(Model => ({ name: Model.collection.name, Model }))
       .filter(({ name }) => !filter || filter.includes(name))
-      .map(x => ({
-        ...x,
-        query: fs.existsSync(x.anchor)
-          ? {
-              _id: {
-                // eslint-disable-next-line
-                $gte: mongoose.Types.ObjectId(
-                  fs.readFileSync(x.anchor, { encoding: "utf8" })
-                )
-              }
-            }
-          : {}
-      }))
-      .map(async ({ name, Model, query, anchor }) => {
-        const count = await Model.count(query);
-
-        console.log(
-          `Indexing model ${name}, count: ${count} query: ${JSON.stringify(
-            query
-          )}`
-        );
-
-        let docCount = 0;
-        Model.on("es-bulk-data", doc => {
-          if (docCount++ >= 10000) {
-            console.log(`Writing ${name} anchor '${doc._id}'`);
-            fs.writeFile(anchor, `${doc._id}`, () => {});
-            docCount = 0;
-          }
-        });
+      .map(async ({ name, Model }) => {
+        console.log(`Indexing model ${name}`);
 
         Model.on("es-bulk-error", err => {
           console.error(`${name} error: `);
           console.error(err);
         });
 
-        await Model.esSynchronize({ $query: query, $orderBy: { _id: 1 } });
+        await Model.esSynchronize();
 
         console.log(`Model ${name} indexed`);
       })
